48d6dda1322ad79bdd0be5ec2e4f48d1432d08d8
KK <741397730@qq.com>
2020-09-22 11:40:21 +0800

    注册发送ajax

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,11 +35,11 @@ form.verify({
     // value: 是获取到的确认密码框中的值
     // item: 就是确认密码框这个标签对象
     // 2.1 获取第一次输入的密码
-    var passVal = $(".registered .pass-first").val();
+    var passVal = $(".registered .pass").val();
     // 判断两次密码是否一样
     if (passVal !== value) {
       // 清空两次输入框
-      $(".registered .pass-first,.registered .pass-second").val("");
+      $(".registered .pass-first,.registered .repass").val("");
       return "两次输入的密码不一致";
     }
   },
@@ -47,4 +47,31 @@ form.verify({
   //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
   // \S 非空字符  \d数字
   pass: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+});
+
+// 3. 实现注册功能
+// 3.1 给form表单注册submit事件
+$(".registered .myForm").on("submit", function (e) {
+  // 阻止表单的默认提交行为
+  e.preventDefault();
+  // 3.3 发送ajax请求
+  $.ajax({
+    type: "post",
+    url: "http://ajax.frontend.itheima.net/api/reguser",
+    // serialize()表单序列化会将 form标签中的所有具备name属性的值一并获取到并拼接成'key=value&key=value'形式的字符串
+    data: $(this).serialize(),
+    success: function (res) {
+      if (res.status == 0) {
+        // 如果注册成功 应该要切换到登陆页面
+        $(".login").show().next().hide();
+      } else {
+        // 如果用户名冲突则要提示
+        layer.open({
+          title: "温馨提示",
+          content: res.message,
+          time: 2000,
+        });
+      }
+    },
+  });
 });
